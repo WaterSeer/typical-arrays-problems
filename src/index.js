a73c269698df02bdc33759ab102f0954fa9b6f51
Mike Kargin <41961514+WaterSeer@users.noreply.github.com>
2021-03-15 09:19:37 +0300

    Update index.js
    Implementing functions min(), max(), avg()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0)
+        return 0;
+    let answer = Number.MAX_SAFE_INTEGER;
+    for (let i =0; i < array.length - 1; i++){
+        if(array[i] < answer)
+            answer = array[i];
+    }
+    return answer;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0)
+        return 0;
+    let answer = Number.MIN_SAFE_INTEGER;
+    for (let i =0; i < array.length; i++){
+        if(array[i] > answer)
+            answer = array[i];
+    }
+    return answer;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0)
+        return 0;
+    let answer = 0;
+    for (let i =0; i < array.length; i++){
+        answer += array[i];
+    }
+    return answer/(array.length);
 }
